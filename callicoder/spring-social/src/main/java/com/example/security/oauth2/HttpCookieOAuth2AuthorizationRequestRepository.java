d17aca5f9ea58ada01604d311fc8ac300858b0c9
alexandrov_v <alexandrov_v@bifit.com>
2019-12-07 15:07:38 +0300

    Spring Boot OAuth2 Social Login with Google, Facebook, and Github - Part 2 https://www.callicoder.com/spring-boot-security-oauth2-social-login-part-2/ - Add Custom classes for OAuth2 Authentication: 4. OAuth2AuthenticationSuccessHandler 5. OAuth2AuthenticationFailureHandler

--- callicoder/spring-social/src/main/java/com/example/security/oauth2/HttpCookieOAuth2AuthorizationRequestRepository.java
+++ callicoder/spring-social/src/main/java/com/example/security/oauth2/HttpCookieOAuth2AuthorizationRequestRepository.java
@@ -1,5 +1,6 @@
 package com.example.security.oauth2;
 
+import com.example.util.CookieUtils;
 import com.nimbusds.oauth2.sdk.util.StringUtils;
 import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
 import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
